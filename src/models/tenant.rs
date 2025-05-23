b4dc136efc457172aefe327cd33d4c7896b92009
sp <hiteshy9904@gmail.com>
2025-05-23 17:20:04 +0530

    add cors, tracing, tenant routes

--- src/models/tenant.rs
+++ src/models/tenant.rs
@@ -5,6 +5,7 @@ use sqlx::FromRow;
 pub struct Tenant {
     pub id: String,
     pub name: String,
+    pub slug: String,
     pub image: String,
     pub description: String,
 }
@@ -12,6 +13,11 @@ pub struct Tenant {
 #[derive(Debug, Deserialize)]
 pub struct TenantRequest {
     pub name: String,
+    pub slug: String,
     pub image: String,
-    pub description: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TenantDescription {
+    pub redirection_url: String,
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,2 +1,3 @@
+pub mod json;
 pub mod middleware;
 pub mod response_utils;
